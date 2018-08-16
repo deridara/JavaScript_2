6af02f7e75855e660dfead86c8ae63228c8b4ed3
dara <teriaderide@gmail.com>
2018-08-16 12:12:08 +0300

    some style changes

--- 3_RegExp/script.js
+++ 3_RegExp/script.js
@@ -1,6 +1,14 @@
 const regExp = /((?<=[.,!? ])'|^'|'(?=\s))/gm;
 const textProcessBtn = document.querySelector('#processBtn');
 const textarea = document.querySelector('textarea');
+
+textarea.value = `'I'm going to steel Anna's cookie,' said the Ghost.
+'Oh no, you can't!' answered Mary.
+'Oh yeah, I can.'
+'No!' Mary cried. 'You can't do it now!'`
+
+
+
 textProcessBtn.addEventListener('click', function() {
     const text = textarea.value;
     textarea.value = text.replace(regExp, '"');
